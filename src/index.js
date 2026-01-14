887e22e098976a5b520a7f3019ae68bc928ce80d
zl <zl@qq.com>
2026-01-14 16:14:12 +0800

    添加 Android 环境必须加载完cordova 才开始渲染

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function renderApp() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+// 如果在 Cordova 环境中，等待 deviceready
+if (window.cordova) {
+  document.addEventListener(
+    'deviceready',
+    () => {
+      console.log('Cordova deviceready');
+      renderApp();
+    },
+    false
+  );
+} else {
+  // 浏览器环境，直接渲染
+  console.log('Web environment, render directly');
+  renderApp();
+};
